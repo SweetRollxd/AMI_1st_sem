fda7099f4072d2602b08930208f35f9b6d4cce98
SweetRollxd <sukharik@ngs.ru>
2022-12-16 01:16:35 +0700

    NMM vector b calculation

--- NMM/lab1/simplified_lab1/main.cpp
+++ NMM/lab1/simplified_lab1/main.cpp
@@ -19,6 +19,11 @@ struct elem {
     vector<point> values;
 };
 
+// функция расчета базисной функции
+// x1 - первая X-координата элемента
+// x2 - вторая X-координата
+// x - X-координата в промежутке между x1 и x2
+// func_index - индекс базисной функции (при нуле пси1, иначе пси2)
 float baseFunc(float x1, float x2, float x, float func_index){
 //     cout << "x1 " << x1 << ", x2 " << x2 << ", x " << x << ", func_index = " << func_index << endl;
      if (func_index == 0) return (x2 - x) / (x2 - x1);
@@ -113,94 +118,62 @@ int main()
 
     }
     dataFile.close();
-    // TODO: граничные элементы должны попадать только в один из интервалов
     cout << "Filled elements:" << endl;
     for(auto it = elemArr.begin(); it != elemArr.end(); it++)
         print_elem(*it);
 //    elemArr
     float globalMatrix[3][3];
     float globalVector[3];
-    // зануляем всю матрицу
-    for (int i = 0; i < elemArr.size() + 1; i++)
+    // зануляем всю матрицу и вектор
+    for (int i = 0; i < elemArr.size() + 1; i++){
+        globalVector[i] = 0;
         for (int j = 0; j < elemArr.size() + 1; j++)
             globalMatrix[i][j] = 0;
+    }
 
+    // рассчет матрицы A
     for (int i = 0; i < elemArr.size(); i++){
         for (int nu = 0; nu < 2; nu++){
             for (int mu = 0; mu < 2; mu++){
                 float matrixCell = 0;
                 for (auto dataInElement = elemArr[i].values.begin(); dataInElement != elemArr[i].values.end(); dataInElement++){
-                    // рассчет локальной матрицы A
+                    // рассчет ячейки матрицы
                     float psinu = baseFunc(elemArr[i].n1, elemArr[i].n2, dataInElement->x, nu);
                     float psimu = baseFunc(elemArr[i].n1, elemArr[i].n2, dataInElement->x, mu);
                     matrixCell += 1 * psinu * psimu;
                 }
-                if (nu == mu) globalMatrix[i + nu][i + mu] += matrixCell;
-                else globalMatrix[i + nu][i + mu] = matrixCell;
+//                std::cout << "globalMatrix[" << i + nu << "][" << i + mu << "] = " << matrixCell << endl;
+                globalMatrix[i + nu][i + mu] += matrixCell;
+//                if (nu == mu) globalMatrix[i + nu][i + mu] += matrixCell;
+//                else globalMatrix[i + nu][i + mu] = matrixCell;
             }
         }
     }
-    //TODO: добавить расчет вектора b
-    cout << "Global matrix " << i << ":" << endl;
+    cout << "Global matrix A:" << endl;
     for (int i = 0; i < elemArr.size() + 1; i++){
         for (int j = 0; j < elemArr.size() + 1; j++){
             cout << globalMatrix[i][j] << " ";
         }
         cout << endl;
     }
-
-    /*
+    // рассчет вектора b
     for (int i = 0; i < elemArr.size(); i++){
-        float localMatrix[2][2];
-        float localVector[2];
         for (int nu = 0; nu < 2; nu++){
-            bool vectorCalculated = false;
-            float vectorCell = 0;
-            for (int mu = 0; mu < 2; mu++){
-
-                float matrixCell = 0;
-                for (auto dataInElement = elemArr[i].values.begin(); dataInElement != elemArr[i].values.end(); dataInElement++){
-                    // рассчет локальной матрицы A
-                    float psinu = baseFunc(elemArr[i].n1, elemArr[i].n2, dataInElement->x, nu);
-                    float psimu = baseFunc(elemArr[i].n1, elemArr[i].n2, dataInElement->x, mu);
-//                    cout << "Psinu " << psinu << ", Psimu " << psimu << endl;
-                    matrixCell += 1 * psinu * psimu;
-
-                    // расчет локального вектора b
-                    if (!vectorCalculated)
-                        vectorCell += 1 * psinu * dataInElement->y;
-                }
-//                cout << "Matrix cell " << "[" << nu << "][" << mu << "] = " << matrixCell << endl;
-                localMatrix[nu][mu] = matrixCell;
+            for (auto dataInElement = elemArr[i].values.begin(); dataInElement != elemArr[i].values.end(); dataInElement++){
+                // рассчет ячейки вектора
+                float psinu = baseFunc(elemArr[i].n1, elemArr[i].n2, dataInElement->x, nu);
+                globalVector[i + nu] += 1 * psinu * dataInElement->y;
             }
-            localVector[nu] = vectorCell;
 
-        }
-        cout << "Local matrix " << i << ":" << endl;
-        for (int m = 0; m < 2; m++){
-            for (int n = 0; n < 2; n++){
-                cout << localMatrix[m][n] << " ";
-            }
-            cout << endl;
-        }
-        for (int n = 0; n < 2; n++){
-            for (int m = 0; m < 2; m++){
-                // здесь неправильно. Надо соединять по принципу, что крайние элементы складываются а те, у которых совпадают номера базисных функций
-                if (n == 0 && m == 0) globalMatrix[i+n][i+m] += localMatrix[n][m];
-                else globalMatrix[i+n][i+m] = localMatrix[n][m];
-//                cout << "Global matrix cell " << "[" << i+n << "][" << i+m << "] = " << localMatrix[n][m] << endl;
-            }
-            cout << endl;
         }
     }
-
+    cout << "Global vector b:" << endl;
     for (int i = 0; i < elemArr.size() + 1; i++){
-        for (int j = 0; j < elemArr.size() + 1; j++){
-            cout << globalMatrix[i][j] << " ";
-        }
-        cout << endl;
+        cout << globalVector[i] << " ";
     }
-    */
+
+
+    // TODO: добавить решение уравнения
 
 //    draw_data(elemArr[0].values);
     return 0;
